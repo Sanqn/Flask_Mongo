211ab2d98d16ada5ceb66b3562aa4153927d04ce
Sanqn <sandisvert@gmail.com>
2022-05-31 14:21:57 +0300

    create update, replace and delete func

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 
 class Config(object):
-    SECRET_URI = os.environ.get("mongodb://localhost:27017/client") or "mongodb://localhost:27017/client"
+    MONGO_URI = os.environ.get("mongodb://localhost:27017/users") or "mongodb://localhost:27017/users"
--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import flask
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+from werkzeug.utils import redirect
 from config import Config
 import json
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/users"
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/users"
+app.config.from_object(Config)
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
-@app.route("/", methods=['get', 'post'])
-def add_user():
-    user_name = ''
-    user_age = ''
 
-    if request.method == 'POST':
-        user_name = request.form.get('name')
-        user_age = request.form.get('age')
-    if user_name and user_age:
-        db.users.insert_one({'name': user_name, 'age': user_age})
-        message = flask.jsonify(message="success")
-        return render_template('add_user.html', message=message)
-    return render_template('add_user.html')
-    # db.users.insert_one({'name': 'Joombo', 'age': 25})
-    # message = flask.jsonify(message="success")
-    # return render_template('add_user.html', message=message)
+# @app.route("/", methods=['get', 'post'])
+# def add_user():
+#     user_name = ''
+#     user_age = ''
+#
+#     if request.method == 'POST':
+#         user_name = request.form.get('name')
+#         user_age = request.form.get('age')
+#     if user_name and user_age:
+#         db.users.insert_one({'name': user_name, 'age': user_age})
+#         message = flask.jsonify(message="success")
+#         return render_template('add_user.html', message=message)
+#     return render_template('add_user.html')
+
+@app.route("/")
+def add_user():
+    db.users.insert_one({'name': 'Root', 'age': 25})
+    message = flask.jsonify(message="success")
+    return message
+
 
 # @app.route("/find_user", methods=['get', 'post'])
 # def find_user_get():
@@ -33,10 +40,11 @@ def add_user():
 #     user = db.users.find({'name': user_name})
 #     json_name = dumps(user)
 #     data = json.loads(json_name)
+#     jom = {}
 #     for i in data:
-#         name = i['name']
-#         age = i['age']
-#         return render_template('find_user.html', name=name, age=age)
+#         jom[i['name']] = jom.get(i['name'], i['age'])
+#     return render_template('find_user.html', jom=jom)
+
 
 @app.route("/find_user/<user_name>", methods=['get', 'post'])
 def find_user_get(user_name):
@@ -44,7 +52,23 @@ def find_user_get(user_name):
     user = dumps(users)
     return user
 
+@app.route("/replace_user/<user_name>")
+def replace_user_get(user_name):
+    user = db.users.find_one_and_replace({'name': user_name}, {'name': 'Jura'})
+    return dumps(user)
 
+
+@app.route("/update_user/<user_name>")
+def update_user_get(user_name):
+    user = db.users.find_one_and_update({'name': user_name}, {"$set": {'age': 50}})
+    return dumps(user)
+
+@app.route("/delete_user/<user_name>")
+def delete_user_get(user_name):
+    user = db.users.find_one_and_delete({'name': user_name})
+    if user is not None:
+        return dumps(user)
+    return "Name does not exist"
 
 if __name__ == "__main__":
     app.run()
